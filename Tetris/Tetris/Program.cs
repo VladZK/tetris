43d784a7e3c04cc9182379206d77b904011a815b
Vlad <v.k.zaitsev@outlook.com>
2021-06-29 15:26:08 +0300

    Вывели 2 точки на экран.

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -11,10 +11,25 @@ namespace Tetris
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello world!!!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
 
+            int x1 = 3;
+            int y1 = 5;
+            char sym = '#';
+
+            Drow(x1, y1, sym);
+           
+        }
+
+        static void Drow(int x, int y, char sym)
+        {
+
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
 
 
         }
+
     }
 }
